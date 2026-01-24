783f6f3ceab60cefa93e7185a329a718f74f3cf3
spacenavy90 <10876259+spacenavy90@users.noreply.github.com>
2026-01-24 12:34:33 -0600

    fix afm1 textures

--- SW_armada/sn-armadaspawner.lua
+++ SW_armada/sn-armadaspawner.lua
@@ -4799,8 +4799,8 @@ Ship:new(SmallShip,SEPARATIST_L_SHIP, {
     Ship:new(MediumShip, REBEL_L_SHIP, {
         name = "Assault Frigate Mark I A [LegacyAlpha]",
         front = "https://images.swarmada.wiki/images/assault-frigate-mark-i-a-legacy-alpha.webp",
-        mesh = LEGACY_ASSETS .. "ships/rebel/afm1/afm1a_mesh.obj",
-        diffuse = LEGACY_ASSETS .. "ships/rebel/afm1/afm1_a_texture.jpg",
+        mesh = CUSTOM_ASSETS .. "ships/rebel/afm1/afm1a_mesh.obj",
+        diffuse = CUSTOM_ASSETS .. "ships/rebel/afm1/afm1a_texture.jpg",
         ruler = CUSTOM_ASSETS .. "ships/republic/dread_gar/ruler.obj",
         maneuver = { { "II" }, { "I", "I" }, { "-", "I", "I" } },
         defense_tokens = { DEF_REDIRECT, DEF_SALVO, DEF_BRACE },
@@ -4813,8 +4813,8 @@ Ship:new(SmallShip,SEPARATIST_L_SHIP, {
     Ship:new(MediumShip, REBEL_L_SHIP, {
         name = "Assault Frigate Mark I B [LegacyAlpha]",
         front = "https://images.swarmada.wiki/images/assault-frigate-mark-i-b-legacy-alpha.webp",
-        mesh = LEGACY_ASSETS .. "ships/rebel/afm1/afm1b_mesh.obj",
-        diffuse = LEGACY_ASSETS .. "ships/rebel/afm1/afm1_b_texture.jpg",
+        mesh = CUSTOM_ASSETS .. "ships/rebel/afm1/afm1b_mesh.obj",
+        diffuse = CUSTOM_ASSETS .. "ships/rebel/afm1/afm1b_texture.jpg",
         ruler = CUSTOM_ASSETS .. "ships/republic/dread_gar/ruler.obj",
         maneuver = { { "II" }, { "I", "I" }, { "-", "I", "I" } },
         defense_tokens = { DEF_REDIRECT, DEF_SALVO, DEF_BRACE },
